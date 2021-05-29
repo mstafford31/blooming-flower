855f5d25851418d475a2d093a9e8c1aff7d0d83c
mstafford31 <80693030+mstafford31@users.noreply.github.com>
2021-05-29 09:36:22 -0400

    Update index.js
    Corrected API key

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "6499912ee915ef0d75da861d4accbf71";
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
